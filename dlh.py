03df08006a4cceeb3987980c981a24b665582735
kritzerenkrieg <kentaro.keren@gmail.com>
2023-12-01 14:12:56 +0700

    First commit with parquet and s3

---
+++ dlh.py
@@ -0,0 +1,49 @@
+import boto3
+import io
+import pandas as pd
+import os
+
+# Set your Minio credentials and endpoint
+minio_credentials = {
+    'region': 'us-east-1',
+    'endpoint': 'http://10.183.16.169:9000',
+    'use_ssl': False,
+    'url_style': 'path',
+    'access_key': 'readonly',
+    'secret_key': 'readonly'
+}
+
+# Create an S3 client
+s3 = boto3.client(
+    's3',
+    region_name=minio_credentials['region'],
+    endpoint_url=minio_credentials['endpoint'],
+    aws_access_key_id=minio_credentials['access_key'],
+    aws_secret_access_key=minio_credentials['secret_key'],
+    use_ssl=minio_credentials['use_ssl'],
+    config=boto3.session.Config(signature_version='s3v4')
+)
+
+# List all buckets
+response = s3.list_buckets()
+
+# Print each bucket name
+print("list of buckets")
+for bucket in response['Buckets']:
+    print(bucket['Name'])
+    objects = s3.list_objects(Bucket=bucket['Name'])
+    for obj in objects['Contents']:
+        print('Downloading ' + obj['Key'])
+        
+        if os.path.isfile(obj['Key'].split('/')[-1] + '.parquet'):
+            print(obj['Key'] + ' already exists, skipping...')
+            continue
+
+        data = io.BytesIO()
+        s3.download_fileobj(bucket['Name'], obj['Key'], data)
+        data = io.StringIO(data.getvalue().decode('utf-8'))
+        
+        print(obj['Key'] + ' downloaded, now turn it into a parquet file')
+
+        df = pd.read_csv(data)
+        df.to_parquet(obj['Key'].split('/')[-1] + '.parquet', index=False)
