c8ab0963865ef3c5a191a0e3703fdc1f098c38e7
unknown <kentaro.keren@gmail.com>
2023-12-05 10:01:10 +0700

    upload create folder if not exist, added example query

---
+++ concat2022to2023_minio.py
@@ -0,0 +1,58 @@
+import io
+import pandas as pd
+from minio import Minio
+from minio.error import S3Error
+
+# Set your Minio server information
+minio_url = "localhost:9000"
+access_key = "minioadmin"
+secret_key = "minioadmin"
+bucket_name = "parquets"
+destination_bucket_name = "queries"
+
+# Create Minio client
+minio_client = Minio(minio_url, access_key, secret_key, secure=False)
+
+# List all objects in the bucket
+objects = minio_client.list_objects(bucket_name)
+
+# Initialize an empty list to store DataFrames
+dfs = []
+
+# Loop through each Parquet file in the bucket
+for obj in objects:
+    # Check if the file name contains "2022" or "2023"
+    if '2022' in obj.object_name or '2023' in obj.object_name:
+        # Download the Parquet file
+        file_data = minio_client.get_object(bucket_name, obj.object_name)
+        parquet_data = io.BytesIO(file_data.read())
+
+        # Read the Parquet file into a DataFrame
+        df = pd.read_parquet(parquet_data)
+
+        # Append the DataFrame to the list
+        dfs.append(df)
+
+# Combine all DataFrames into a single DataFrame
+combined_df = pd.concat(dfs, ignore_index=True)
+
+# Display the combined DataFrame
+print(combined_df)
+
+# Check if destination bucket exists, create it if not
+if not minio_client.bucket_exists(destination_bucket_name):
+    minio_client.make_bucket(destination_bucket_name)
+
+# Upload query to bucket
+try:
+    # Convert the DataFrame to Parquet format
+    parquet_data = io.BytesIO()
+    combined_df.to_parquet(parquet_data, index=False)
+    # Reset the buffer position to the beginning
+    parquet_data.seek(0)
+    # Upload the Parquet data to the destination bucket
+    minio_client.put_object(destination_bucket_name, 'annual_data.parquet', parquet_data, parquet_data.getbuffer().nbytes)
+    print("'Annual Data' parquet uploaded to 'queries' bucket successfully!")
+
+except ReferenceError as err:
+    print(f"Error uploading data: {err}")
--- uploadparquet_minio.py
+++ uploadparquet_minio.py
@@ -1,7 +1,8 @@
+import os
 from minio import Minio
-from minio.error import ResponseError
+from minio.error import S3Error
 
-def upload_to_minio(file_path, object_name, bucket_name, endpoint, access_key, secret_key):
+def upload_parquet_files_to_minio(folder_path, bucket_name, endpoint, access_key, secret_key):
     try:
         # Initialize the Minio client
         minio_client = Minio(endpoint,
@@ -13,23 +14,28 @@ def upload_to_minio(file_path, object_name, bucket_name, endpoint, access_key, s
         if not minio_client.bucket_exists(bucket_name):
             minio_client.make_bucket(bucket_name)
 
-        # Upload the file
-        minio_client.fput_object(bucket_name, object_name, file_path)
+        # Loop through files in the specified directory
+        for filename in os.listdir(folder_path):
+            if filename.endswith(".parquet"):
+                file_path = os.path.join(folder_path, filename)
+                object_name = filename
 
-        print(f"File '{object_name}' uploaded successfully to bucket '{bucket_name}' on Minio server.")
+                # Upload the parquet file
+                minio_client.fput_object(bucket_name, object_name, file_path)
 
-    except ResponseError as e:
-        print(f"Error: {e}")
+                print(f"File '{object_name}' uploaded successfully to bucket '{bucket_name}' on Minio server.")
+
+    except ReferenceError as e:
+        print(f"Error: {e}" +"Too bad!")
 
 # Set your Minio server details
-minio_endpoint = "http://localhost:9000"  # Update with your Minio server endpoint
-minio_access_key = "your_access_key"
-minio_secret_key = "your_secret_key"
-minio_bucket_name = "your_bucket_name"
+minio_endpoint = "localhost:9000"
+minio_access_key = "minioadmin"
+minio_secret_key = "minioadmin"
+minio_bucket_name = "parquets"
 
-# File to upload
-file_path_to_upload = "/path/to/your/file.txt"
-object_name_in_minio = "uploaded_file.txt"
+# Directory containing parquet files
+parquet_folder_path = "./data"
 
 # Call the function
-upload_to_minio(file_path_to_upload, object_name_in_minio, minio_bucket_name, minio_endpoint, minio_access_key, minio_secret_key)
+upload_parquet_files_to_minio(parquet_folder_path, minio_bucket_name, minio_endpoint, minio_access_key, minio_secret_key)
